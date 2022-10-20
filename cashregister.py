4d1ee5dc7364ac7f1474f2e2a9dd7740a7466a34
NathanDarienTanner <nathan,tanner001@binus.ac.id>
2022-10-20 17:55:15 +0700

    cash register

---
+++ cashregister.py
@@ -0,0 +1,17 @@
+def cashregister(price, cash):
+    bills = [100000, 50000, 20000, 10000, 5000, 2000, 1000, 500, 200, 100]
+    money = []
+    change = cash - price
+    x = 0
+    while True:
+        if x >= 10:
+            break
+        if change >= bills[x]:
+            money.append(bills[x])
+            change -= bills[x]
+        elif change < bills[x]:
+            x += 1
+    return money
+
+
+print(cashregister(65400, 100000))
